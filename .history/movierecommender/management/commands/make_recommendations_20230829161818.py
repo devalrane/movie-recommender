1d4d73740a193a204c079e1352a99f617520557c
Deval Rane <devalrane.28@gmail.com>
2023-08-29 16:21:59 -0400

    Added recommender script

---
+++ .history/movierecommender/management/commands/make_recommendations_20230829161818.py
@@ -0,0 +1,42 @@
+from django.core.management import BaseCommand
+from ...models import Movie
+
+
+# Check if genres are valid
+def check_valid_genres(genres: str) -> bool:
+    if bool(genres and not genres.isspace()) and genres != "na":
+        return True
+    else:
+        return False
+
+
+# more similar in intersection = more similar
+def jaccard_similarity(list1: list, list2: list) -> float:
+    s1 = set(list1)
+    s2 = set(list2)
+    return float(len(s1.intersection(s2)) / len(s1.union(s2)))
+
+
+# similarity is based on genres for now
+def similarity_between_movies(movie1: Movie, movie2: Movie) -> float:
+    if check_valid_genres(movie1.genres) and check_valid_genres(movie2.genres):
+        m1_generes = movie1.genres.split()
+        m2_generes = movie2.genres.split()
+        return jaccard_similarity(m1_generes, m2_generes)
+    else:
+        return 0
+
+
+class Command(BaseCommand):
+    help = "Recommend movies"
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **kwargs):
+        # Figure the recommended field for each unwatched movie
+        # Based on the similarity on movie genres
+        pass
+
+
+# python manage.py make_recommendations
